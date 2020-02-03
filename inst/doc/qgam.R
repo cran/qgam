7020661ea163bb0fc0d18df1d776eb118c717001
Matteo Fasiolo <matteo.fasiolo@gmail.com>
2020-02-03 18:40:02 +0000

    version 1.3.2

--- inst/doc/qgam.R
+++ inst/doc/qgam.R
@@ -1,8 +1,8 @@
-## ----setup, include=FALSE-----------------------------------------------------
+## ----setup, include=FALSE------------------------------------------------
 library(knitr)
 opts_chunk$set(out.extra='style="display:block; margin: auto"', fig.align="center", tidy=FALSE)
 
-## ----1, message = F-----------------------------------------------------------
+## ----1, message = F------------------------------------------------------
 library(qgam); library(MASS)
 if( suppressWarnings(require(RhpcBLASctl)) ){ blas_set_num_threads(1) } # Optional
 
@@ -18,10 +18,10 @@ lines(xSeq$times, pred$fit, lwd = 1)
 lines(xSeq$times, pred$fit + 2*pred$se.fit, lwd = 1, col = 2)
 lines(xSeq$times, pred$fit - 2*pred$se.fit, lwd = 1, col = 2)   
 
-## ----2------------------------------------------------------------------------
+## ----2-------------------------------------------------------------------
 check(fit$calibr, 2)
 
-## ----3, message = F-----------------------------------------------------------
+## ----3, message = F------------------------------------------------------
 set.seed(6436)
 cal <- tuneLearn(accel~s(times, k=20, bs="ad"), 
                  data = mcycle, 
@@ -31,14 +31,14 @@ cal <- tuneLearn(accel~s(times, k=20, bs="ad"),
                  
 check(cal)
 
-## ----4------------------------------------------------------------------------
+## ----4-------------------------------------------------------------------
 quSeq <- c(0.2, 0.4, 0.6, 0.8)
 set.seed(6436)
 fit <- mqgam(accel~s(times, k=20, bs="ad"), 
              data = mcycle, 
              qu = quSeq)
 
-## ----5------------------------------------------------------------------------
+## ----5-------------------------------------------------------------------
 # Plot the data
 xSeq <- data.frame(cbind("accel" = rep(0, 1e3), "times" = seq(2, 58, length.out = 1e3)))
 plot(mcycle$times, mcycle$accel, xlab = "Times", ylab = "Acceleration", ylim = c(-150, 80))
@@ -49,11 +49,11 @@ for(iq in quSeq){
   lines(xSeq$times, pred, col = 2)
 }
 
-## ----6------------------------------------------------------------------------
+## ----6-------------------------------------------------------------------
 # Summary for quantile 0.4
 qdo(fit, qu = 0.4, summary)
 
-## ----h1-----------------------------------------------------------------------
+## ----h1------------------------------------------------------------------
 set.seed(651)
 n <- 2000
 x <- seq(-4, 3, length.out = n)
@@ -69,7 +69,7 @@ qus <- seq(0.05, 0.95, length.out = 5)
 plot(x, dat, col = "grey", ylab = "y")
 for(iq in qus){ lines(x, qnorm(iq, f, sigma)) }
 
-## ----h2-----------------------------------------------------------------------
+## ----h2------------------------------------------------------------------
 fit <- mqgam(y~s(x, k = 30, bs = "cr"), 
              data = dataf,
              qu = qus)
@@ -82,14 +82,14 @@ for(iq in qus){
 }
 legend("top", c("truth", "fitted"), col = 2:1, lty = rep(1, 2))
 
-## ----h3-----------------------------------------------------------------------
+## ----h3------------------------------------------------------------------
 plot(x, dat, col = "grey", ylab = "y")
 tmp <- qdo(fit, 0.95, predict, se = TRUE)
 lines(x, tmp$fit)
 lines(x, tmp$fit + 3 * tmp$se.fit, col = 2)
 lines(x, tmp$fit - 3 * tmp$se.fit, col = 2)
 
-## ----h4-----------------------------------------------------------------------
+## ----h4------------------------------------------------------------------
 fit <- qgam(list(y~s(x, k = 30, bs = "cr"), ~ s(x, k = 30, bs = "cr")), 
             data = dataf, qu = 0.95)
 
@@ -99,7 +99,7 @@ lines(x, tmp$fit)
 lines(x, tmp$fit + 3 * tmp$se.fit, col = 2)
 lines(x, tmp$fit - 3 * tmp$se.fit, col = 2)
 
-## ----mcy2rnd, message = F-----------------------------------------------------
+## ----mcy2rnd, message = F------------------------------------------------
 fit <- qgam(accel~s(times, k=20, bs="ad"), 
             data = mcycle, 
             qu = 0.8)
@@ -112,7 +112,7 @@ lines(xSeq$times, pred$fit, lwd = 1)
 lines(xSeq$times, pred$fit + 2*pred$se.fit, lwd = 1, col = 2)
 lines(xSeq$times, pred$fit - 2*pred$se.fit, lwd = 1, col = 2)   
 
-## ----mcy2rnd2, message = F----------------------------------------------------
+## ----mcy2rnd2, message = F-----------------------------------------------
 fit <- qgam(list(accel ~ s(times, k=20, bs="ad"), ~ s(times)),
             data = mcycle, 
             qu = 0.8)
@@ -123,7 +123,7 @@ lines(xSeq$times, pred$fit, lwd = 1)
 lines(xSeq$times, pred$fit + 2*pred$se.fit, lwd = 1, col = 2)
 lines(xSeq$times, pred$fit - 2*pred$se.fit, lwd = 1, col = 2)  
 
-## ----c1-----------------------------------------------------------------------
+## ----c1------------------------------------------------------------------
 library(qgam)
 set.seed(15560)
 n <- 1000
@@ -134,52 +134,52 @@ y <- drop(X %*% beta) + rnorm(n)
 dataf <- data.frame(cbind(y, x, z))
 names(dataf) <- c("y", "x", "z")
 
-## ----c2-----------------------------------------------------------------------
+## ----c2------------------------------------------------------------------
 qu <- 0.5
 fit <- qgam(y~x, qu = qu, data = dataf)
 cqcheck(obj = fit, v = c("x"), X = dataf, y = y) 
 
-## ----c3, message = F----------------------------------------------------------
+## ----c3, message = F-----------------------------------------------------
 fit <- qgam(y~s(x), qu = qu, data = dataf)
 cqcheck(obj = fit, v = c("x"), X = dataf, y = y)
 
-## ----c4, message = F----------------------------------------------------------
+## ----c4, message = F-----------------------------------------------------
 cqcheck(obj = fit, v = c("x", "z"), X = dataf, y = y, nbin = c(5, 5))
 
-## ----c5, message = F----------------------------------------------------------
+## ----c5, message = F-----------------------------------------------------
 cqcheck(obj = fit, v = c("z"), X = dataf, y = y, nbin = c(10))
 
-## ----c6, message = F----------------------------------------------------------
+## ----c6, message = F-----------------------------------------------------
 fit <- qgam(y~s(x)+z, qu = qu, data = dataf)
 cqcheck(obj = fit, v = c("z"))
 
-## ----c7, message = F----------------------------------------------------------
+## ----c7, message = F-----------------------------------------------------
 cqcheck(obj = fit, v = c("x", "z"), nbin = c(5, 5))
 
-## ----c8, message = F----------------------------------------------------------
+## ----c8, message = F-----------------------------------------------------
 fit <- qgam(y~s(x)+z+I(x*z), qu = qu, data = dataf)
 cqcheck(obj = fit, v = c("x", "z"), nbin = c(5, 5))
 
-## ----c9, message = F----------------------------------------------------------
+## ----c9, message = F-----------------------------------------------------
 fit <- mqgam(y~s(x)+z+I(x*z), qu = c(0.2, 0.4, 0.6, 0.8), data = dataf)
 
-## ----c10, message = F---------------------------------------------------------
+## ----c10, message = F----------------------------------------------------
 check.learnFast(fit$calibr, 2:5)
 
-## ----c11, message = F---------------------------------------------------------
+## ----c11, message = F----------------------------------------------------
 qdo(fit, 0.2, check)
 
-## ----edf1---------------------------------------------------------------------
+## ----edf1----------------------------------------------------------------
 data("UKload")
 tmpx <- seq(UKload$Year[1], tail(UKload$Year, 1), length.out = nrow(UKload)) 
 plot(tmpx, UKload$NetDemand, type = 'l', xlab = 'Year', ylab = 'Load')
 
-## ----edf2---------------------------------------------------------------------
+## ----edf2----------------------------------------------------------------
 qu <- 0.5
 form <- NetDemand~s(wM,k=20,bs='cr') + s(wM_s95,k=20,bs='cr') + 
         s(Posan,bs='ad',k=30,xt=list("bs"="cc")) + Dow + s(Trend,k=4) + NetDemand.48 + Holy
 
-## ----edf3, message=FALSE------------------------------------------------------
+## ----edf3, message=FALSE-------------------------------------------------
 set.seed(41241)
 sigSeq <- seq(4, 8, length.out = 16)
 closs <- tuneLearn(form = form, data = UKload, 
@@ -188,12 +188,12 @@ closs <- tuneLearn(form = form, data = UKload,
 
 check(closs)
 
-## ----edf4---------------------------------------------------------------------
+## ----edf4----------------------------------------------------------------
 lsig <- closs$lsig
 fit <- qgam(form = form, data = UKload, lsig = lsig, qu = qu)
 plot(fit, scale = F, page = 1)
 
-## ----edf5---------------------------------------------------------------------
+## ----edf5----------------------------------------------------------------
 par(mfrow = c(2, 2))
 cqcheck(fit, v = c("wM"), main = "wM")
 cqcheck(fit, v = c("wM_s95"), main = "wM_s95")
@@ -202,7 +202,7 @@ cqcheck(fit, v = c("Trend"), main = "Trend", xaxt='n')
 axis(1, at = UKload$Trend[c(1, 500, 1000, 1500, 2000)], 
              UKload$Year[c(1, 500, 1000, 1500, 2000)] )
 
-## ----edf6---------------------------------------------------------------------
+## ----edf6----------------------------------------------------------------
 par(mfrow = c(1, 1))
 cqcheck(fit, v = c("wM", "Posan"), scatter = T)
 
